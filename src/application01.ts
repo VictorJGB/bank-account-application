6a5fedc1211871e998f496cff9ca4cf094ee281a
Victor Jerrysson <victorjerrysson@gmail.com>
2023-07-15 16:30:23 -0300

    feat: Adding the application 01 logic

--- src/application01.ts
+++ src/application01.ts
@@ -1,5 +1,27 @@
-import CurrentAccount from './models/CurrentAccount';
+import Employee from './models/Employee';
 
-const account01 = new CurrentAccount(2000, '082-023-745-193');
+const employee01 = new Employee(
+  2500.0,
+  '000.000.000-01',
+  'Antônio',
+  '(88) 9 9111-2292',
+  'Gerente'
+);
+const employee02 = new Employee(
+  1800.0,
+  '000.000.000-02',
+  'Rafael',
+  '(88) 9 9222-3393',
+  'Atendente'
+);
 
-console.log(account01);
+const showEmployee = (employee: Employee) => {
+  console.log('\nFuncionário:', employee.Name);
+  console.log('CPF:', employee.CPF);
+  console.log('Telefone:', employee.Phone);
+  console.log('Salário:', employee.Salary);
+  console.log('Cargo:', employee.Office);
+};
+
+showEmployee(employee01);
+showEmployee(employee02);
